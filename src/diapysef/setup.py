0043160a4c03ac560882fd2226c987e102b2d691
mffrank <max.florian.frank@gmail.com>
2018-04-25 09:41:04 -0400

    Update setup.py to include scripts in install

--- src/diapysef/setup.py
+++ src/diapysef/setup.py
@@ -3,12 +3,17 @@ from setuptools import setup
 setup(name='diapysef',
       version='0.1',
       description='Analysis and visualization of DIA PASEF data',
-      author='Max Frank',
-      author='Hannes Roest',
+      author='Max Frank, Hannes Roest',
       author_email='max.frank@mail.utoronto.ca',
       license='MIT',
       packages=['diapysef'],
+      install_requires=[
+          'pandas',
+          'numpy'],
       package_data={
           'diapysef': ['data/*']
-      }
+      },
+      scripts=['scripts/get_dia_windows.py',
+               'scripts/annotate_mq_ionmobility.py',
+               'scripts/plot_dia_windows.py'],
       zip_safe=False)
