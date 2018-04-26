fe93a3848bc40133bd06b0f43b0ee98bf90924aa
Hannes Roest <hroest@stanford.edu>
2018-04-26 14:47:03 -0400

    [FIX] fix script

--- src/diapysef/scripts/plot_dia_windows.py
+++ src/diapysef/scripts/plot_dia_windows.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
-from diapysef.plotting import plot_window_layout
-import sys
-import pandas as pd
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
 """
 Plots the window layout of a tims experiment with precursor density of a pasef experiment.
 """
+import sys
 if len(sys.argv) < 3:
     print("Usage: plot_dia_windows.py window_file precursor_map")
+    sys.exit()
+
+from diapysef.plotting import plot_window_layout
+import pandas as pd
 
 windows = pd.read_csv(sys.argv[1])
 precursors = pd.read_csv(sys.argv[2])
