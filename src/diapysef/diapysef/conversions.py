3335ad411cfc07d10b54584129d2a1f31596f603
Annie Ha <annieha@nia-login01.scinet.local>
2019-04-23 15:44:21 -0400

    Extracted alignment functions

--- src/diapysef/diapysef/conversions.py
+++ src/diapysef/diapysef/conversions.py
@@ -7,6 +7,7 @@ import numpy as np
 from statsmodels.graphics.regressionplots import abline_plot
 from matplotlib.backends.backend_pdf import PdfPages
 from scipy.interpolate import interp1d
+import statsmodels.api as smnonlinear
 
 def calibrate(data, plot = True, pdf = "rtcalibration"):
     """Fits a linear model to the irts"""
@@ -23,8 +24,6 @@ def calibrate(data, plot = True, pdf = "rtcalibration"):
     a.savefig(pdf, format = 'pdf')
     matplotlib.pyplot.close()
     return [filename, res.params.Intercept, res.params.rt]
-    # row = np.array([['raw', 'intercept', 'slope'], [data.iloc[0,0], res.params.Intercept, res.params.rt]])
-    # return pd.DataFrame(data = row[1:,], columns = row[0,0:])
 
 def outliers(data, tolerance = 7):
     '''
@@ -44,17 +43,105 @@ def outliers(data, tolerance = 7):
 def get_product_charge(msl, msms):
     return(msl)
 
+def align_rt(msms_irt, ms, runs, rt_alignment, pdfout, remove_outliers = True):
+    """Retention time alignment"""
+    if rt_alignment is 'linear':
+        print('Aligning retention time linearly ...')
+        # Generate the iRT calibrators
+        calibrators = []
+        pp = PdfPages(pdfout)
+        for file in runs:
+            cal = calibrate(msms_irt[msms_irt.raw == file], pdf = pp)
+            calibrators.append(cal)
+        pp.close()
+        # Apply rt calibrators
+        calibrators = pd.DataFrame(columns = ['Raw file', 'intercept', 'slope'], data = calibrators)
+        ms = pd.merge(ms, calibrators, on = 'Raw file')
+        ms['irt'] = ms.intercept + ms.slope * ms['Calibrated retention time']
+        ms = ms.drop(columns=['intercept', 'slope'])
+        
+    elif rt_alignment is 'nonlinear':
+        print('Aligning retention time with lowess fitting ...')
+        lowess = smnonlinear.nonparametric.lowess
+        for file in runs:
+            msms_irt_sub = msms_irt[msms_irt.raw == file]
+            msms_irt_sub = msms_irt_sub.loc[:,["rt","irt"]
+            if remove_outliers is True:
+                msms_irt_sub = outliers(msms_irt_sub)[0]
+            # Calculate span
+            delta = (max(msms_irt_sub['rt'] - min(msms_irt_sub['rt'])) * 0.01 )
+            if len(msms_irt_sub) < 100:
+                frac = 1.0
+            else:
+                frac = 0.1
+
+            # lowess fits by y/x
+            r = lowess(msms_irt_sub['irt'], msms_irt_sub['rt'], delta=delta, frac=frac)
+            lowess_x = list(zip(*r))[0]
+            lowess_y = list(zip(*r))[1]
+            # create an interpolation function
+            f = interp1d(lowess_x, lowess_y, bounds_error=False)
+            nRT = np.asarray(f(ms.loc[ms['Raw file'] == file ,'Calibrated retention time'].values))
+            
+            # fit linear extrapolation for values outside of approximation
+            # print(sum(np.isnan(nRT)))
+            min_bound = min(lowess_x)
+            max_bound = max(lowess_x)
+
+            idx = np.asarray(np.where((ms.loc[ms['Raw file'] == file, 'Calibrated retention time'].values < min_bound) | (ms.loc[ms['Raw file'] == file, 'Calibrated retention time'].values > max_bound)))   
+
+            lnmod = calibrate(msms_irt_sub)
+            intercept = lnmod[1] # intercept
+            slope = lnmod[2] # slope
+            nRT[idx] = slope * (ms.loc[ms['Raw file'] == file, 'Calibrated retention time'].values[idx]) + intercept
+            nrt = []
+            for t in nRT: nrt.append(t)
+            ms.loc[ms['Raw file'] == file, 'irt'] = list(map(str, nrt))
+    else:
+        print('Only rt_alignment: linear and lowess calibrations are currently impletmented.')
+        ms['irt'] = ms['Calibrated retention time']
+
+    return(ms)
+
+
+def align_im(msms_irt, ms, runs, im_alignment):
+    """Ion mobility calibration."""
+    if im_alignment is 'linear':
+        print('Aligning ion mobility linearly...')
+        # Generate the iIM calibrators
+        calibrators = []
+        pp = PdfPages('imcalibration.pdf')
+        for file in runs:
+            msms_irt_sub = msms_irt[msms_irt.raw == file]
+            msms_irt_sub = msms_irt_sub.loc[:,["raw","sequence","im","iim"]]
+            msms_irt_sub.columns = ["raw","sequence","rt","irt"]
+            cal = calibrate(msms_irt_sub, pdf = pp)
+            calibrators.append(cal)
+        pp.close()
+        # Apply im calibrators
+        calibrators = pd.DataFrame(columns = ['Raw file', 'intercept', 'slope'], data = calibrators)
+        ms = pd.merge(ms, calibrators, on = 'Raw file')
+        ms['iim'] = ms.intercept + ms.slope * ms['imcol']
+        ms = ms.drop(columns=['intercept', 'slope'])
+    else:
+        print("Only im_alignment='linear' is currently implemented")
+        ms['iim'] = ms['imcol']
+    return(ms)
+
 def pasef_to_tsv(evidence, msms,
                  irt_file = None,
+                 ion_mobility = None,
                  pdfout = "rtcalibration.pdf",
-                 im_column = 'Ion mobility index',
+                 im_column = 'IonMobilityIndexK0',
                  rt_alignment = 'nonlinear',
                  im_alignment = 'linear'):
     """Converts a mq output to a library taking a best replicate approach."""
-
-    ev = evidence.loc[:, ["id", "Calibrated retention time", "Ion mobility index", im_column]]
+    if ion_mobility is not None:    
+        ev = evidence.loc[:, ["id", "Calibrated retention time", "Ion mobility index", im_column]]
+        ev = ev.rename(columns = {im_column:'imcol'})
+    else:
+        ev = evidence.loc[:, ["id", "Calibrated retention time"]]
     ev = ev.rename(columns = {'id':'Evidence ID'})
-    ev = ev.rename(columns = {im_column:'imcol'})
     ms = pd.merge(msms, ev, on = 'Evidence ID')
     ms = ms.dropna(subset=["Calibrated retention time"]) # Some precursors in MQ are not annotated with a RT
 
@@ -80,7 +167,10 @@ def pasef_to_tsv(evidence, msms,
             irt = irt.loc[:, ["ModifiedPeptideSequence", "NormalizedRetentionTime", "PrecursorIonMobility","PrecursorCharge"]]
             irt = irt.drop_duplicates()
 
-        irt.columns = ["sequence","irt", "iim", "charge"]
+        if ion_mobility is not None:
+            irt.columns = ["sequence","irt", "iim", "charge"]
+        else:
+            irt.columns = ["sequence","irt", "charge"]
         irt['sequence'] = irt['sequence'].str.replace("\\(UniMod:4\\)","C(Carbamidomethyl)")
         irt['sequence'] = irt['sequence'].str.replace("\\(UniMod:1\\)","Acetylation")
         irt['sequence'] = irt['sequence'].str.replace("\\(UniMod:35\\)","Oxidation")
@@ -90,95 +180,24 @@ def pasef_to_tsv(evidence, msms,
         msms_irt = ms[ms["ModifiedPeptideSequence"].isin(irt["sequence"])]
         ## We chose the best id of each peptide (only one charge state for alignment)
         msms_irt = msms_irt.loc[msms_irt.groupby(["Raw file","ModifiedPeptideSequence"])['PEP'].idxmin()]
-        msms_irt = msms_irt.loc[:, ["Raw file","ModifiedPeptideSequence","Calibrated retention time", "imcol", "Charge"]]
-        msms_irt.columns = ["raw","sequence","rt","im", "charge"]
+
+        if ion_mobility is not None:
+            msms_irt = msms_irt.loc[:, ["Raw file","ModifiedPeptideSequence","Calibrated retention time", "imcol", "Charge"]]
+            msms_irt.columns = ["raw","sequence","rt","im", "charge"]
+        else:
+            msms_irt = msms_irt.loc[:, ["Raw file","ModifiedPeptideSequence","Calibrated retention time", "Charge"]]
+            msms_irt.columns = ["raw","sequence","rt", "charge"]
+
         msms_irt = pd.merge(msms_irt, irt, on = ['sequence', 'charge'])
         raw_files = msms_irt.raw.unique()
        
         if rt_alignment is not None:
-            if rt_alignment is 'linear':
-                print("Aligning retention time linearly...")
-                # Generate the iRT calibrators
-                calibrators = []
-                pp = PdfPages(pdfout)
-                for file in raw_files:
-                    cal = calibrate(msms_irt[msms_irt.raw == file], pdf = pp)
-                    calibrators.append(cal)
-                pp.close()
-
-                # Apply rt calibrators
-                calibrators = pd.DataFrame(columns = ['Raw file', 'intercept', 'slope'], data = calibrators)
-                ms = pd.merge(ms, calibrators, on = 'Raw file')
-                ms['irt'] = ms.intercept + ms.slope * ms['Calibrated retention time']
-                ms = ms.drop(columns=['intercept', 'slope'])
-
-            elif rt_alignment is 'nonlinear':
-                print("Aligning retention time by lowess...")
-                import statsmodels.api as smnonlinear
-                lowess = smnonlinear.nonparametric.lowess
-                # make a df to store the fitted values for merging later
-                for file in raw_files:
-                    msms_irt_sub = msms_irt[msms_irt.raw == file]
-                    msms_irt_sub = msms_irt_sub.loc[:,["rt","irt"]]
-                    # does not need to remove outliers
-                    # span for the lowess interpolation is 1% of the range
-                    delta = (max(msms_irt_sub['rt'] - min(msms_irt_sub['rt'])) * 0.01 )
-                    if len(msms_irt_sub) < 100:
-                        frac = 1.0 # take the fraction of data to generate the weighted fitting
-                    else:
-                        frac = 0.1
-                    # lowess fits by y/x
-                    r = lowess(msms_irt_sub['irt'], msms_irt_sub['rt'], delta=delta, frac=frac)
-                    lowess_x = list(zip(*r))[0]
-                    lowess_y = list(zip(*r))[1]
-                    # create an interpolation function
-                    f = interp1d(lowess_x, lowess_y, bounds_error=False)
-                    nRT = np.asarray(f(ms.loc[ms['Raw file'] == file ,'Calibrated retention time'].values))
-
-                    # fit linear extrapolation for values outside of approximation
-                    # print(sum(np.isnan(nRT)))
-                    min_bound = min(lowess_x)
-                    max_bound = max(lowess_x)
-                    idx = np.asarray(np.where((ms.loc[ms['Raw file'] == file, 'Calibrated retention time'].values < min_bound) | (ms.loc[ms['Raw file'] == file, 'Calibrated retention time'].values > max_bound)))
-
-                    # cannot use pandas.index on ms, it returns the index of the original ms data frame and not th subsetted dataframe
-
-                    lnmod = calibrate(msms_irt_sub)
-                    intercept = lnmod[1] # intercept
-                    slope = lnmod[2] # slope
-                    nRT[idx] = slope * (ms.loc[ms['Raw file'] == file, 'Calibrated retention time'].values[idx]) + intercept
-     
-                    nrt = []
-                    for t in nRT: nrt.append(t)
-                    ms.loc[ms['Raw file'] == file, 'irt'] = list(map(str, nrt))
-            else:
-                print("Only rt_alignment:linear and lowess calibrations are currently implemented")
-                ms['irt'] = ms['Calibrated retention time']
+            ms = align_rt(msms_irt, ms, raw_files, rt_alignment, pdfout, remove_outliers = True)
         else:
             ms['irt'] = ms['Calibrated retention time']
 
         if im_alignment is not None:
-            if im_alignment is "linear":
-                print("Aligning ion mobility")
-                # Generate the iIM calibrators
-                calibrators = []
-                pp = PdfPages("imcalibration.pdf")
-                for file in raw_files:
-                    msms_irt_sub = msms_irt[msms_irt.raw == file]
-                    msms_irt_sub = msms_irt_sub.loc[:,["raw","sequence","im","iim"]]
-                    msms_irt_sub.columns = ["raw","sequence","rt","irt"]
-                    cal = calibrate(msms_irt_sub, pdf = pp)
-                    calibrators.append(cal)
-                pp.close()
-                # Apply im calibrators
-                calibrators = pd.DataFrame(columns = ['Raw file', 'intercept', 'slope'], data = calibrators)
-                ms = pd.merge(ms, calibrators, on = 'Raw file')
-                ms['iim'] = ms.intercept + ms.slope * ms['imcol']
-                ms = ms.drop(columns=['intercept', 'slope'])
-            else:
-                print("Only im_alignment='linear' is currently implemented")
-                ms['iim'] = ms['imcol']
-
+            ms = align_im(msms_irt, ms, raw_files, im_alignment)
         else:
             ms['iim'] = ms['imcol']
     else:
@@ -189,15 +208,24 @@ def pasef_to_tsv(evidence, msms,
     ms = ms.loc[ms.groupby(["ModifiedPeptideSequence", "Charge"])['PEP'].idxmin()]
     # Filter decoy peptides
     ms = ms[ms['Reverse'].isna()]
-
+    
+    if ion_mobility is not None:
     # Shape data for AssayGenerator
-    msl = ms.loc[:, ["id","m/z","Masses","Charge","irt", "iim", "imcol", "Intensities","Sequence","ModifiedPeptideSequence","Proteins"]]
+        msl = ms.loc[:, ["id","m/z","Masses","Charge","irt", "iim", "imcol", "Intensities","Sequence","ModifiedPeptideSequence","Proteins"]]
+    else:
+        msl = ms.loc[:, ["id","m/z","Masses","Charge","irt", "Intensities","Sequence","ModifiedPeptideSequence","Proteins"]]
+
     masses = msl['Masses'].str.split(';', expand = True).stack().str.strip().reset_index(level = 1, drop=True)
     intensities = msl['Intensities'].str.split(';', expand = True).stack().str.strip().reset_index(level = 1, drop=True)
     df1 = pd.concat([masses, intensities], axis = 1, keys = ['Masses', 'Intensities'])
     msl2 = msl.drop(['Masses', 'Intensities'], axis = 1)
     msl2 = msl2.join(df1).reset_index(drop = True)
-    msl2.columns = ["transition_group_id","PrecursorMz","PrecursorCharge","iRT", "Im_recalibrated", "PrecursorIonMobility", "PeptideSequence","FullUniModPeptideName","ProteinName", "ProductMz", "LibraryIntensity"]
+
+    if ion_mobility is not None:
+        msl2.columns = ["transition_group_id","PrecursorMz","PrecursorCharge","iRT", "Im_recalibrated", "PrecursorIonMobility", "PeptideSequence","FullUniModPeptideName","ProteinName", "ProductMz", "LibraryIntensity"]
+    else:
+        msl2.columns = ["transition_group_id","PrecursorMz","PrecursorCharge","iRT", "PeptideSequence","FullUniModPeptideName","ProteinName", "ProductMz", "LibraryIntensity"]
+
     msl2 = get_product_charge(msl2, msms)
 
     # Reorder the columns as they are in libraries generated with the OSW assay generator
